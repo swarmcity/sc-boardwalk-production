648d06bdb6a440c8c2d8940b6aca06090409fd01
paleov <admin@swarmcity.io>
2017-07-09 10:47:24 -0700

    arc token and first test

--- truffle.js
+++ truffle.js
@@ -7,5 +7,23 @@ module.exports = {
       port: 8545,
       network_id: '*',
     },
+    ropsten: {
+      host: "localhost",
+      port: 8545,
+      gas: 4710000,
+      network_id: "*"
+    },
+    kovan: {
+      host: "localhost",
+      port: 8545,
+      gas: 2710000,
+      network_id: "*"
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      gas: 4710000,
+      network_id: "4"
+    },
   },
 };
