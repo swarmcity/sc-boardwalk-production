f9bb68ffdd842c317b729f4979f73bfdc084a764
@bkawk <email.will.in.china@gmail.com>
2017-07-17 16:18:38 +0800

    add keythereum

--- browserify.js
+++ browserify.js
@@ -1,5 +1,6 @@
 module.exports = {
     util: require('ethereumjs-util'),
     web3: require('web3'),
-    tx: require('ethereumjs-tx')
+    tx: require('ethereumjs-tx'),
+    key: require("keythereum")
 }
