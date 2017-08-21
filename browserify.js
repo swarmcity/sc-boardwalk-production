9f762b8f9869883c8023fc5c9eeded20f8c864b8
@bkawk <email.will.in.china@gmail.com>
2017-08-21 10:38:53 +0700

    base install

--- browserify.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    util: require('ethereumjs-util'),
-    Web3: require('web3'),
-    tx: require('ethereumjs-tx'),
-    key: require("keythereum"),
-    time: require("moment")
-}
