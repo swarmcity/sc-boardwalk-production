0829ddf69b74cf3e204e4bb94e81c451b17c4cf8
@bkawk <email.will.in.china@gmail.com>
2017-08-22 12:56:07 +0800

    add hashtag

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+exports.addHashtag = functions.https.onRequest((req, res) => { 
+    const data = {
+        hashtag_name: req.query.hashtag_name,
+        hashtag_id: req.query.hashtag_id,
+        location: req.query.location,
+        deals: req.query.deals,
+        description: req.query.description,
+        maintainer: req.query.maintainer,
+        contacts: {
+            name: req.query.name,
+            link: req.query.link
+        }
+    }
+    admin.database().ref('/hashtags').push(data)
+    .then(snapshot => {
+    res.redirect(303, snapshot.ref);
+    });
+});
+
