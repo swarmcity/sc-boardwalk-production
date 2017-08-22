667dff66e7b3a10ff16e0aa22c4d72baa3b3a8d4
@bkawk <email.will.in.china@gmail.com>
2017-08-22 15:25:04 +0800

    update ipfs hash

--- functions/index.js
+++ functions/index.js
@@ -2,23 +2,34 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-
+// https://us-central1-swarmcity-23c70.cloudfunctions.net/addHashtag?hashtag_name=pioneer&hashtag_id=1&location=ew234e&deals=0&description=This is a description&maintainer=theKing&name=twitter&link=http://www.twitter.com
 exports.addHashtag = functions.https.onRequest((req, res) => { 
+    let ipfs_hash = '';
     const data = {
-        hashtag_name: req.query.hashtag_name,
-        hashtag_id: req.query.hashtag_id,
-        location: req.query.location,
-        deals: req.query.deals,
-        description: req.query.description,
-        maintainer: req.query.maintainer,
         contacts: {
             name: req.query.name,
-            link: req.query.link
-        }
+            link: req.query.link,
+        },
+        deals: req.query.deals,
+        description: req.query.description,
+        hashtag_id: req.query.hashtag_id,
+        hashtag_name: req.query.hashtag_name,
+        ipfs_hash:0,
+        location: req.query.location,
+        maintainer: req.query.maintainer,
     }
     admin.database().ref('/hashtags').push(data)
     .then(snapshot => {
-    res.redirect(303, snapshot.ref);
+        ipfs_hash = data2Hash(data);
+        return snapshot.ref.toString().split("/")[4]; 
+    })
+    .then(data1 => {
+        admin.database().ref('/hashtags/'+data1).update({ipfs_hash:ipfs_hash})
     });
 });
 
+
+function data2Hash(data) {
+    // GET IPFS HASH FOR DATA HERE
+    return 'aaaaa1aaaa'
+};
