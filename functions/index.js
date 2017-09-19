6325ac7d47522b59f8217220072b18213748d04c
Stefaan Ponnet <stefaan@ponnet.com>
2017-09-19 22:05:03 +0200

    added IPFS to firebase function..

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,13 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const uuidv4 = require('uuid/v4');
+const ipfsAPI = require('ipfs-api');
+var ipfs = ipfsAPI({
+              host: 'ipfsapi.swarm.city',
+              protocol: 'https',
+              port : 443
+            });
+
 admin.initializeApp(functions.config().firebase);
 
 // https://us-central1-swarmcity-23c70.cloudfunctions.net/addHashtag?hashtag_name=pioneer&last_deal=1503397878&deals=0&description=This is a description&maintainer=this hash tag is maintained by Swarm City&name=twitter&link=http://www.twitter.com
@@ -22,17 +29,29 @@ exports.addHashtag = functions.https.onRequest((req, res) => {
     }
     admin.database().ref('/hashtags').push(data)
     .then(snapshot => {
-        ipfs_hash = data2Hash(data);
         return snapshot.ref.toString().split("/")[4]; 
     })
     .then(data1 => {
-        admin.database().ref('/hashtags/'+data1).update({ipfs_hash:ipfs_hash});
-        res.status(200).send('OK');
+        // TODO for the will : convert this to a promise.
+        data2Hash(data).then( ipfs_hash => {
+            admin.database().ref('/hashtags/'+data1).update({ipfs_hash:ipfs_hash});
+            res.status(200).send('OK');
+        });
     });
 });
 
 
-function data2Hash(data) {
+function data2Hash(data,cb) {
+        var bufferdata = new Buffer(JSON.stringify(data);
+        return new Promise((resolve, reject) => {
+            ipfs.add(bufferdata, function(err, res) {
+                if (err){
+                    return reject();
+                }
+                return resolve(res);
+            });
+        });
+
     // GET IPFS HASH FOR DATA HERE
-    return 'QmV8G4EzLq9AMvrw7f9kdjzdPsGefyjrCp6hnP7urWa8ED'
+    //return 'QmV8G4EzLq9AMvrw7f9kdjzdPsGefyjrCp6hnP7urWa8ED'
 };
