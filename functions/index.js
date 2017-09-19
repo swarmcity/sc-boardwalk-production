a3903629046a86ebfd6b4ab5b485c88ddf79489e
@bkawk <email.will.in.china@gmail.com>
2017-09-19 14:06:14 +0800

    add request function

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,17 @@ const admin = require('firebase-admin');
 const uuidv4 = require('uuid/v4');
 admin.initializeApp(functions.config().firebase);
 
+/**
+* @param {string} hashtag_name a budget you are willing to pay for for your request
+* @param {string} last_deal a description of what you are requesting
+* @param {string} deals a description of what you are requesting
+* @param {string} last_deal a description of what you are requesting
+* @param {string} description a description of what you are requesting
+* @param {string} maintainer a description of what you are requesting
+* @param {string} name a description of what you are requesting
+* @param {string} link a description of what you are requesting
+* @return {string} a 200 OK is returned
+*/
 // https://us-central1-swarmcity-23c70.cloudfunctions.net/addHashtag?hashtag_name=pioneer&last_deal=1503397878&deals=0&description=This is a description&maintainer=this hash tag is maintained by Swarm City&name=twitter&link=http://www.twitter.com
 exports.addHashtag = functions.https.onRequest((req, res) => { 
     const hashtag_id = uuidv4();
@@ -31,8 +42,37 @@ exports.addHashtag = functions.https.onRequest((req, res) => {
     });
 });
 
+/**
+* @param {string} budget a budget you are willing to pay for for your request
+* @param {string} lookingFor a description of what you are requesting
+* @param {string} location a geohash
+* @param {string} hashtagId a geohash
+* @return {string} a 200 OK is returned
+*/
+// https://us-central1-swarmcity-23c70.cloudfunctions.net/addRequest?budget=150&lookingFor=A%20Place%20To%20Stay&location=k3lj45
+exports.addRequest = functions.https.onRequest((req, res) => {
+    const request_id = uuidv4();
+    let ipfs_hash = 'awaiting hash';
+    const data = {
+        looking_for: req.query.lookingFor,
+        budget: req.query.budget,
+        location: req.query.location,
+        hashtag_id: req.query.hashtagId,
+    }
+    admin.database().ref('/requests').push(data)
+    .then(snapshot => {
+        ipfs_hash = data2Hash(data);
+        return snapshot.ref.toString().split("/")[4]; 
+    })
+    .then(data1 => {
+        admin.database().ref('/requests/'+data1).update({ipfs_hash:ipfs_hash});
+        res.status(200).send('OK');
+    });
+})
 
 function data2Hash(data) {
     // GET IPFS HASH FOR DATA HERE
-    return 'QmV8G4EzLq9AMvrw7f9kdjzdPsGefyjrCp6hnP7urWa8ED'
+    return 'AmV1G4EzLq9AMvrw7f9kdjzdPsGefyjrCp6hnP7urWa8EX'
 };
+
+
