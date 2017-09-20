c14e275bc184f92be395450c390a24706266e30d
Michael Thuy <michael@thuy.be>
2017-09-20 18:03:05 +0200

    GetBalances with firebase
    yaay!

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const uuidv4 = require('uuid/v4');
+const Web3 = require('web3');
+const minime = require('./IMiniMeToken.json');
+
 admin.initializeApp(functions.config().firebase);
 
 /**
@@ -15,7 +18,7 @@ admin.initializeApp(functions.config().firebase);
 * @return {string} a 200 OK is returned
 */
 // https://us-central1-swarmcity-23c70.cloudfunctions.net/addHashtag?hashtag_name=pioneer&last_deal=1503397878&deals=0&description=This is a description&maintainer=this hash tag is maintained by Swarm City&name=twitter&link=http://www.twitter.com
-exports.addHashtag = functions.https.onRequest((req, res) => { 
+exports.addHashtag = functions.https.onRequest((req, res) => {
     const hashtag_id = uuidv4();
     let ipfs_hash = 'awaiting hash';
     const data = {
@@ -34,7 +37,7 @@ exports.addHashtag = functions.https.onRequest((req, res) => {
     admin.database().ref('/hashtags').push(data)
     .then(snapshot => {
         ipfs_hash = data2Hash(data);
-        return snapshot.ref.toString().split("/")[4]; 
+        return snapshot.ref.toString().split("/")[4];
     })
     .then(data1 => {
         admin.database().ref('/hashtags/'+data1).update({ipfs_hash:ipfs_hash});
@@ -63,7 +66,7 @@ exports.addRequest = functions.https.onRequest((req, res) => {
     admin.database().ref('/requests').push(data)
     .then(snapshot => {
         ipfs_hash = data2Hash(data);
-        return snapshot.ref.toString().split("/")[4]; 
+        return snapshot.ref.toString().split("/")[4];
     })
     .then(data1 => {
         admin.database().ref('/requests/'+data1).update({ipfs_hash:ipfs_hash});
@@ -73,7 +76,7 @@ exports.addRequest = functions.https.onRequest((req, res) => {
 
 /**
 * @param {string} offer a budget you are willing to offer for for the request
-* @param {string} requestId the promary key from a request 
+* @param {string} requestId the promary key from a request
 * TODO: We need to check this requestId exists
 * @param {string} reply a description of the reply to an offer
 * @return {string} a 200 OK is returned
@@ -90,7 +93,7 @@ exports.addReply = functions.https.onRequest((req, res) => {
     admin.database().ref('/replies').push(data)
     .then(snapshot => {
         ipfs_hash = data2Hash(data);
-        return snapshot.ref.toString().split("/")[4]; 
+        return snapshot.ref.toString().split("/")[4];
     })
     .then(data1 => {
         admin.database().ref('/replies/'+data1).update({ipfs_hash:ipfs_hash});
@@ -98,9 +101,67 @@ exports.addReply = functions.https.onRequest((req, res) => {
     });
 })
 
+exports.getUserbalances = functions.database.ref('/connected/{publickey}')
+    .onWrite(event => {
+      const original = event.data.val();
+      //console.log('Uppercasing', event.params.publickey, original);
+      var livebalances = getBalances(event.params.publickey).then(function(livebalances){
+        console.log(livebalances);
+        // Livebalances = {swt: values[0], arc: values[1], eth:values[2]};
+        admin.database().ref('/usercache/'+event.params.publickey).set(livebalances);
+      });
+});
+
+
 function data2Hash(data) {
     // GET IPFS HASH FOR DATA HERE
     return 'AmV1G4EzLq9AMvrw7f9kdjzdPsGefyjrCp6hnP7urWa8EX'
-};
+}
 
+function getBalances(pubkey) {
 
+  var web3 = new Web3();
+
+  web3.setProvider(new web3.providers.HttpProvider('https://ethpool.swarm.city'));
+
+  //console.log(web3, web3.eth);
+  // Get the current SWT and ETH balances
+
+  var promise_swt = new Promise((resolve, reject) => {
+
+    var swtminime = web3.eth.contract(minime.abi);
+    var swtminimeInstance = swtminime.at("0xb9e7f8568e08d5659f5d29c4997173d84cdf2607");
+
+    swtminimeInstance.balanceOf(pubkey, function(err, res) {
+      if (!err) {
+        //console.log('swt: ', res.toNumber(10));
+        resolve(res.toNumber(10));
+      }
+    });
+  });
+
+  var promise_arc = new Promise((resolve, reject) => {
+
+    var arcminime = web3.eth.contract(minime.abi);
+    var arcminimeInstance = arcminime.at("0xAc709FcB44a43c35F0DA4e3163b117A17F3770f5");
+
+    arcminimeInstance.balanceOf(pubkey, function(err, res) {
+      if (!err) {
+        resolve(res.toNumber(10));
+      }
+    });
+  });
+
+  var promise_eth = new Promise((resolve, reject) => {
+    web3.eth.getBalance(pubkey, function(err, res) {
+      resolve(res.toNumber(10));
+    });
+  });
+
+  return new Promise((resolve, reject) => {
+    Promise.all([promise_swt, promise_arc, promise_eth]).then(values => {
+      resolve({swt: values[0], arc: values[1], eth:values[2]});
+    });
+  });
+
+}
